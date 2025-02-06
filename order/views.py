8029d691d5fa90af6e3abf59cf43fb3d043c9fd1
kipsang <kipsang1998@gmail.com>
2025-02-06 22:30:44 +0300

    wip: add views

--- order/views.py
+++ order/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Customer, Order
+from .serializers import CustomerSerializer, OrderSerializer
+from rest_framework import permissions
+
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated]
+
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    
+    def perform_create(self, serializer):
+        order = serializer.save()
+        order.send_notification()
+        return order
